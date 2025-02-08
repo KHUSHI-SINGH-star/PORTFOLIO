555b02654500cb0fd0d547af93e83bfbbe7de05e
KHUSHI-SINGH-star <khushisingh977374@gmail.com>
2025-02-08 02:33:32 +0000

    add project section

--- portfolio.js
+++ portfolio.js
@@ -4,11 +4,12 @@ const Name=document.querySelector("name_section");
 const email=document.querySelector(".email_section");
 const phoneNo=document.querySelector(".phoneNO_section");
 const massage=document.querySelector(".massage_section");
-// const ig=document.querySelector("#ig")
+ const ig=document.querySelector("#ig")
 // const ln=document.querySelector("#li")
  const github=document.querySelector("#github")
 // const gmail=document.querySelector("#gmail");
 const tap_button=document.querySelector(".last");
+const more_about_me=document.querySelector("#more_about_me");
 
 
 // const name1= Name.value;
@@ -23,9 +24,9 @@ last_button.addEventListener("click",()=>{
 window.scrollTo({top:1,behaviour:"instat"})
 })
 
-// ig.addEventListener("click",()=>{
-//     document.location="https://www.instagram.com/"
-// })
+ ig.addEventListener("click",()=>{
+     document.location="https://www.instagram.com/"
+ })
 
 // gmail.addEventListener("click",()=>{
 //     // document.location=""
@@ -41,4 +42,8 @@ github.addEventListener("click",()=>{
 
 tap_button.addEventListener("click",()=>{
     window.scrollTo({top:1,behavior:"smooth"})
+})
+
+more_about_me.addEventListener("click",()=>{
+    window.scrollTo({bottom:200,behavior:"smooth"})
 })
